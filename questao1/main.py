b0a604eb0dc4395ef1492b42fc4ea79aa49bb883
Miguel F Laurentino <mfl15@aluno.ifal.edu.br>
2026-06-09 10:21:39 -0300

    feat: arquivo principal para execucao da atividade

--- questao1/main.py
+++ questao1/main.py
@@ -1,7 +1,26 @@
 from tabulate import tabulate
+from sum import sum
+from subtraction import subtraction
+from multiply import multiply
+from division import division
 
 
 def exibir_mensagem():
+    print("Calculadora simples em Python")
+
+
+exibir_mensagem()
+print()
+
+
+def values():
+    a = float(input("Digite o primeiro número para a operação: "))
+    b = float(input("Digite o segundo número para a operação: "))
+    return (a, b)
+
+
+def main() -> None:
+
     data = [
         ["1", "Soma"],
         ["2", "Subtração"],
@@ -13,8 +32,52 @@ def exibir_mensagem():
     try:
         print(tabulate(data, headers=[" ", "Opção"], tablefmt="simple"))
         opcao = int(input("Qual operação deseja fazer? "))
+        
+        if opcao < 1 or opcao > 5:
+            raise ValueError()
+    
+        match opcao:
+            case 1:
+                a, b = values()
+                print(sum(a, b))
+                print()
+                main()
+            case 2:
+                a, b = values()
+                print(subtraction(a, b))
+                print()
+                main()
+            case 3:
+                a, b = values()
+                print(multiply(a, b))
+                print()
+                main()
+            case 4:
+                try:
+                    a, b = values()
+                    if b == 0:
+                        raise ZeroDivisionError()
+
+                    print(division(a, b))
+                except ZeroDivisionError:
+                    print("Você não pode digitar um número por zero.\nTente novamente.")
+                finally:
+                    print()
+                    main()
+            case 5:
+                return print("Fim do programa!")
+            case _:
+                raise ValueError()
+
+
     except Exception as e:
-        print(e)
+        print()
+        print("=-" * 15 + "=")
+        print("Você digitou um valor inválido.\nTente novamente.")
+        print("=-" * 15 + "=")
+        print()
+
+        main()
 
 
-exibir_mensagem()
+main()
