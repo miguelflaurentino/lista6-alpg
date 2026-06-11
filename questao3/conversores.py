72b2ad9a9151b9b1ec917876baa4bdc48f4f7c87
Miguel Laurentino <mfl15@aluno.ifal.edu.br>
2026-06-11 09:50:58 -0300

    feat: logica da questao tres

---
+++ questao3/conversores.py
@@ -0,0 +1,6 @@
+def celsius_para_fahrenheit(celsius: int | float) -> float:
+    return celsius * 1.8 + 32
+
+
+def fahrenheit_para_celsius(fahrenheit: int | float) -> float:
+    return (fahrenheit - 32) * 1.8
---
+++ questao3/programa.py
@@ -0,0 +1,4 @@
+import conversores
+
+print(conversores.celsius_para_fahrenheit(30))
+print(conversores.fahrenheit_para_celsius(86))
